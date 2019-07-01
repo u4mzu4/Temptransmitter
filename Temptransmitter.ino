ea97dc7081f79fecc0af41d5972206189c16137f
andrew911 <39950568+u4mzu4@users.noreply.github.com>
2019-07-01 20:45:50 +0200

    Initial update

---
+++ Temptransmitter.ino
@@ -0,0 +1,84 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS D7
+#define DS18B20_RESOLUTION 11
+#define SENSORPIN A0
+#define TIMEOUT 5000
+#define REFRESHTIME 60000
+#define SERVER_PORT 80
+
+const char* ssid     = "DarpAsusNet_2.4";
+const char* password = "andrew243";
+
+char tempstr[5];
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensor(&oneWire);
+DeviceAddress sensorDeviceAddress0;
+DeviceAddress sensorDeviceAddress1;
+ESP8266WebServer server(SERVER_PORT);
+
+void handleRoot() {
+  server.send(200, "text/plain", tempstr);
+  server.sendHeader("Connection", "close");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+}
+
+void setup() {
+  unsigned long wifitimeout;
+  // put your setup code here, to run once:
+  sensor.begin();
+  sensor.getAddress(sensorDeviceAddress0, 0);
+  sensor.getAddress(sensorDeviceAddress1, 1);
+  sensor.setResolution(sensorDeviceAddress0, DS18B20_RESOLUTION);
+  sensor.setResolution(sensorDeviceAddress1, DS18B20_RESOLUTION);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin (ssid, password);
+  // Wait for connection
+  wifitimeout = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    if (millis()-wifitimeout > TIMEOUT)
+    {
+      break;
+    }
+  }
+  server.on("/", handleRoot);
+  server.begin(SERVER_PORT);
+}
+
+void loop() {
+  unsigned short tempRaw0;
+  unsigned short tempRaw1;
+  static unsigned long timer;
+  int sensorValue;
+  float tempC;
+  //read sensor
+
+  server.handleClient();
+  if (millis()-timer>REFRESHTIME)
+  {
+    sensor.requestTemperaturesByAddress(sensorDeviceAddress0);
+    tempRaw0 = sensor.getTemp(sensorDeviceAddress0);
+    while (tempRaw0 == 0x2A80)
+    {
+      sensor.requestTemperaturesByAddress(sensorDeviceAddress0);
+      tempRaw0 = sensor.getTemp(sensorDeviceAddress0);
+    }
+    sensor.requestTemperaturesByAddress(sensorDeviceAddress1);
+    tempRaw1 = sensor.getTemp(sensorDeviceAddress1);
+    while (tempRaw1 == 0x2A80)
+    {
+      sensor.requestTemperaturesByAddress(sensorDeviceAddress1);
+      tempRaw1 = sensor.getTemp(sensorDeviceAddress1);
+    }
+    tempC=min(tempRaw0,tempRaw1)/128.0;
+    sensorValue = analogRead(SENSORPIN);
+    dtostrf(tempC, 5, 1, tempstr);
+    timer=millis();
+  }
+}
