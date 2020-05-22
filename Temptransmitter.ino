612ef30efce33c5494e171bfc75b0728198e713d
andrew911 <39950568+u4mzu4@users.noreply.github.com>
2020-05-22 22:50:08 +0200

    Change HW to Sonoff S26
    for aesthetic reasons

--- Temptransmitter.ino
+++ Temptransmitter.ino
@@ -2,39 +2,75 @@
 #include <ESP8266WebServer.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <credentials.h>
 
-#define ONE_WIRE_BUS D7
+#define ONE_WIRE_BUS 12
+#define LEDPIN 13
 #define DS18B20_RESOLUTION 11
-#define SENSORPIN A0
 #define TIMEOUT 5000
-#define REFRESHTIME 60000
+#define REFRESHTIME 50000
+#define LEDON 500
 #define SERVER_PORT 80
+#define NR_OF_SENSORS 2
 
-const char* ssid     = "";
-const char* password = "";
-
-char tempstr[5];
+char tempStr[5];
+unsigned long lastRefresh;
+bool ledIsOn;
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensor(&oneWire);
-DeviceAddress sensorDeviceAddress0;
-DeviceAddress sensorDeviceAddress1;
+DeviceAddress sensorDeviceAddress[NR_OF_SENSORS];
 ESP8266WebServer server(SERVER_PORT);
 
-void handleRoot() {
-  server.send(200, "text/plain", tempstr);
+void handleRoot()
+{
+  server.send(200, "text/plain", tempStr);
   server.sendHeader("Connection", "close");
   server.sendHeader("Access-Control-Allow-Origin", "*");
+  digitalWrite(LEDPIN, LOW);
+  ledIsOn = 1;
+  lastRefresh = millis();
 }
 
-void setup() {
+void ReadDS18B20()
+{
+  unsigned short tempRaw[NR_OF_SENSORS];
+  static unsigned long timer;
+  float tempC;
+  unsigned long DS18B20timeout;
+
+  //read sensors
+  for (int j = 0; j < NR_OF_SENSORS; j++)
+  {
+    sensor.requestTemperaturesByAddress(sensorDeviceAddress[j]);
+    tempRaw[j] = sensor.getTemp(sensorDeviceAddress[j]);
+    DS18B20timeout = millis();
+    while (tempRaw[j] == 0x2A80)
+    {
+      sensor.requestTemperaturesByAddress(sensorDeviceAddress[j]);
+      tempRaw[j] = sensor.getTemp(sensorDeviceAddress[j]);
+      if (millis() - DS18B20timeout > TIMEOUT)
+      {
+        break;
+      }
+    }
+  }
+  tempC = min(tempRaw[0], tempRaw[1]) / 128.0;
+  dtostrf(tempC, 5, 1, tempStr);
+}
+
+void setup()
+{
   unsigned long wifitimeout;
-  // put your setup code here, to run once:
+
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, HIGH);
   sensor.begin();
-  sensor.getAddress(sensorDeviceAddress0, 0);
-  sensor.getAddress(sensorDeviceAddress1, 1);
-  sensor.setResolution(sensorDeviceAddress0, DS18B20_RESOLUTION);
-  sensor.setResolution(sensorDeviceAddress1, DS18B20_RESOLUTION);
+  for (int i = 0; i < NR_OF_SENSORS; i++)
+  {
+    sensor.getAddress(sensorDeviceAddress[i], i);
+    sensor.setResolution(sensorDeviceAddress[i], DS18B20_RESOLUTION);
+  }
 
   WiFi.mode(WIFI_STA);
   WiFi.begin (ssid, password);
@@ -42,54 +78,28 @@ void setup() {
   wifitimeout = millis();
   while (WiFi.status() != WL_CONNECTED) {
     delay(200);
-    if (millis()-wifitimeout > TIMEOUT)
+    if (millis() - wifitimeout > TIMEOUT)
     {
       break;
     }
   }
+  ReadDS18B20();
   server.on("/", handleRoot);
   server.begin(SERVER_PORT);
 }
 
-void loop() {
-  unsigned short tempRaw0;
-  unsigned short tempRaw1;
+void loop()
+{
   static unsigned long timer;
-  int sensorValue;
-  float tempC;
-  unsigned long DS18B20timeout;
-  //read sensor
 
   server.handleClient();
-  if (millis()-timer>REFRESHTIME)
+  if ((millis() - lastRefresh > LEDON) && ledIsOn)
   {
-    sensor.requestTemperaturesByAddress(sensorDeviceAddress0);
-    tempRaw0 = sensor.getTemp(sensorDeviceAddress0);
-    DS18B20timeout = millis();
-    while (tempRaw0 == 0x2A80)
-    {
-      sensor.requestTemperaturesByAddress(sensorDeviceAddress0);
-      tempRaw0 = sensor.getTemp(sensorDeviceAddress0);
-      if (millis()-DS18B20timeout > TIMEOUT)
-        {
-        break;
-        }
-    }
-    sensor.requestTemperaturesByAddress(sensorDeviceAddress1);
-    tempRaw1 = sensor.getTemp(sensorDeviceAddress1);
-    DS18B20timeout = millis();
-    while (tempRaw1 == 0x2A80)
-    {
-      sensor.requestTemperaturesByAddress(sensorDeviceAddress1);
-      tempRaw1 = sensor.getTemp(sensorDeviceAddress1);
-      if (millis()-DS18B20timeout > TIMEOUT)
-        {
-        break;
-        }
-    }
-    tempC=min(tempRaw0,tempRaw1)/128.0;
-    sensorValue = analogRead(SENSORPIN);
-    dtostrf(tempC, 5, 1, tempstr);
-    timer=millis();
+    digitalWrite(LEDPIN, HIGH);
+    ledIsOn = 0;
+  }
+  if (millis() - lastRefresh > REFRESHTIME)
+  {
+    ReadDS18B20();
   }
 }
